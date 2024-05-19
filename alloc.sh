44ee10b309e7b588bc08a928d56020cc03c5de61
leopoldh <leopold.hermansson@gmail.com>
2024-05-19 18:22:54 +0200

    .

---
+++ alloc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+# time allocation
+#SBATCH -A edu24.sf2568
+# 10 minutes wall-clock time will be given to this job
+#SBATCH -t 00:60:00
+# Number of tasks per core (prevent hyperthreading)
+#SBATCH --ntasks-per-core=1
+#SBATCH --cpus-per-task=1
+# Use Dardel's main partition
+#SBATCH -p shared
+
+# job name
+# Number of nodes
+#SBATCH --nodes=1
+# Number of tasks per node
+#SBATCH --ntasks-per-node=32
+srun ./a.out
---
+++ compile_dardel.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+CC -std=c++20 -O3 src/main_mpi.cpp
