7b8a373c65e28a80dbc8334d2a11a0232d358aeb
leopoldh <leopold.hermansson@gmail.com>
2024-05-20 21:47:17 +0200

    .

--- parse_output.py
+++ parse_output.py
@@ -1,3 +1,5 @@
+from math import log
+from matplotlib import pyplot as plt
 from dataclasses import dataclass
 from pathlib import Path
 import re
@@ -20,6 +22,7 @@ Load: 17ms
 Render: 30ms
 Communication: 173ms"""
 
+
 @dataclass
 class Data:
     data_per_process: int
@@ -31,31 +34,58 @@ class Data:
     rendering: int
     communication: int
 
+
 data: list[Data] = []
 
-for p in Path('.').glob("slurm-394*.out"):
-    data.extend(list(map(lambda x: Data(*map(int, x)), re.findall(re_match, p.read_text()))))
-
-from matplotlib import pyplot as plt
-
-from math import log
-
-fig, ax = plt.subplots(3,1)
+for p in Path('.').glob("slurm-394196*.out"):
+    data.extend(list(map(lambda x: Data(*map(int, x)),
+                re.findall(re_match, p.read_text()))))
 
 
-ax[0].loglog([d.processes for d in data], [d.loading for d in data], '.')
-ax[0].loglog([d.processes for d in data], [d.rendering for d in data], '.')
-ax[0].legend(['Loading', 'Rendering'])
-ax[0].set_title('Loading and rendering time')
+plt.plot([d.processes for d in data], [d.load_positions for d in data], '.')
+plt.plot([d.processes for d in data], [d.sorting for d in data], '.')
+plt.plot([d.processes for d in data], [d.loading for d in data], '.')
+plt.plot([d.processes for d in data], [d.rendering for d in data], '.')
+plt.plot([d.processes for d in data], [d.communication for d in data], '.')
+plt.legend(["load_positions",
+            "sorting",
+            "loading",
+            "rendering",
+            "communication"])
+plt.savefig("all.png")
+plt.clf()
 
+plt.loglog([d.processes for d in data], [d.load_positions for d in data], '.')
+plt.loglog([d.processes for d in data], [d.sorting for d in data], '.')
+plt.loglog([d.processes for d in data], [d.loading for d in data], '.')
+plt.loglog([d.processes for d in data], [d.rendering for d in data], '.')
+plt.loglog([d.processes for d in data], [d.communication for d in data], '.')
+plt.legend(["load_positions",
+            "sorting",
+            "loading",
+            "rendering",
+            "communication"])
+plt.savefig("all_loglog.png")
+plt.clf()
 
-ax[1].plot([d.processes for d in data], [(d.loading)/d.data_per_process for d in data], '.')
-ax[1].plot([d.processes for d in data], [(d.rendering)/d.data_per_process for d in data], '.')
-ax[1].legend(['Loading', 'Rendering'])
-ax[1].set_title('Loading and rendering time / amound of data')
+plt.loglog([d.processes for d in data], [d.loading for d in data], '.')
+plt.loglog([d.processes for d in data], [d.rendering for d in data], '.')
+plt.legend(['Loading', 'Rendering'])
+plt.title('Loading and rendering time')
+plt.savefig("load_render.png")
+plt.clf()
 
-ax[2].plot([d.processes for d in data], [ d.communication for d in data], '.')
-ax[2].set_title('Communication time')
+plt.plot([d.processes for d in data], [
+         (d.loading)/d.data_per_process for d in data], '.')
+plt.plot([d.processes for d in data], [
+         (d.rendering)/d.data_per_process for d in data], '.')
+plt.legend(['Loading', 'Rendering'])
+plt.title('Loading and rendering time / amound of data')
+plt.savefig("load_render_per_data.png")
+plt.clf()
 
-fig.legend()
-plt.show()
+plt.plot([d.processes for d in data], [d.communication for d in data], '.')
+plt.title('Communication time')
+
+plt.savefig("comm.png")
+plt.clf()
